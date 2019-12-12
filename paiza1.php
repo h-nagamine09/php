62e041eb2c77d5b0cc024151fe11a6f6f9e8c651
Hiroki Nagamine <hiroki.nagamine@gmail.com>
2019-12-12 16:58:09 +0900

    PHPの初歩の初歩

---
+++ paiza1.php
@@ -0,0 +1,44 @@
+<?php //開始タグ
+
+echo "paiza php講座";
+
+?>
+ <!-- echoで出力 -->
+ <!-- ;を忘れない -->
+
+ <?php
+// Here your code !
+echo "勇者はレベルアップした";
+
+?>
+<?php
+echo "paizaでプログラミングをマスター！";
+?>
+
+<?php
+// hello paiza と出力してみよう！
+echo "hello paiza";
+?>
+
+<!-- コメントは// -->
+<!-- 変数$ -->
+<?php
+// Here your code !
+$output1 = "hello";
+$output2 = "world";
+echo $output1." ".$output2
+?>
+
+<?php
+$text1 = "勇者は";
+$text2 = "レベルアップした！！";
+// $text1と$text2を結合して出力するプログラムを書いてください。
+echo $text1.$text2
+
+?>
+<?php
+$text1 = "魔物が";
+$text2 = "あらわれた！！";
+echo $text1.$text2;
+
+?>
